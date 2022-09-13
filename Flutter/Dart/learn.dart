e2c842d4cd9b990c7bbd35d956b6c3b236e6b07d
Shiffana Nazeema A <shiffananazeema134@gmail.com>
2022-09-13 08:21:02 +0530

    Add

--- Flutter/Dart/learn.dart
+++ Flutter/Dart/learn.dart
@@ -1,10 +1,3 @@
-// Define a function.
-void printInteger(int aNumber) {
-  print('The number is $aNumber.'); // Print to console.
-}
-
-// This is where the app starts executing.
 void main() {
-  var number = 42; // Declare and initialize a variable.
-  printInteger(number); // Call a function.
+  print("Hello World!");
 }
