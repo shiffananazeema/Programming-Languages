73d635ffcada135a3f207a12cf3cfa538cbebc6d
Shiffana Nazeema A <shiffananazeema134@gmail.com>
2022-10-15 19:46:30 +0530

    Align

--- Flutter/Dart/learn.dart
+++ Flutter/Dart/learn.dart
@@ -1,3 +1,5 @@
-void main() {
+// Started to Learn Dart After Long Time Wasted - 15/10/2022 - 7.30 P.M
+
+main() {
   print("Hello World!");
 }
---
+++ Flutter/first_rough/android/app/src/main/kotlin/com/example/first_rough/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first_rough
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
